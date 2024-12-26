d40c9ad894793e5808e3ffb9c867fb40f105a2bb
Muhammad51 <anvarovm733@gmail.com>
2024-12-27 00:03:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// function numCatch(a) {
+//     let inp=dacument.getElementById('input1');
+//     inp.value = inp.value + a.value;
+// }
+// function barobar() {
+//     let r=dacument.getElementById('input1');
+//     r.value = eval(r.value);
+// }
+// function reset() {
+//     let res=dacument.getElementById('input1');
+//     res.value = "";
+// }
+
+
+
+function numCatch(a){
+    let inp=document.getElementById('input1');
+    inp.value = inp.value + a.value;
+}
+
+function barobar(){
+    let r=document.getElementById('input1');
+    r.value = eval(r.value);
+}
+
+function reset(){
+    let res=document.getElementById('input1');
+    res.value =('')
+}
